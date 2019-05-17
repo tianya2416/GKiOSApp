41f857c584b47a5480935cc4ec97abadde2bd924
wangws1990 <wangws1990@163.com>
2019-05-17 11:46:53 +0800

    bug fix

--- GKiOSApp/GKiOSApp/Class/Home/Model/GKSearchModel.h
+++ GKiOSApp/GKiOSApp/Class/Home/Model/GKSearchModel.h
@@ -9,7 +9,7 @@
 #import "BaseModel.h"
 #import "GKHomeHotModel.h"
 NS_ASSUME_NONNULL_BEGIN
-@class GKSearchItemsModel;
+@class GKSearchItemsModel,GKSearchBaseModel;
 @interface GKSearchModel : BaseModel
 @property (copy, nonatomic) NSString *userId;
 @property (copy, nonatomic) NSString *searchKey;
@@ -22,13 +22,38 @@ NS_ASSUME_NONNULL_BEGIN
 @end
 
 @interface GKSearchItemsModel : BaseModel
+
 @property (copy, nonatomic) NSString *title;
 @property (assign, nonatomic) NSInteger total;
 @property (assign, nonatomic) NSInteger type;
-@property (strong, nonatomic) NSArray <GKHomeHotPaperModel *>*items;
+@property (strong, nonatomic) NSArray <GKSearchBaseModel *>*items;
+
 @end
 
+@interface GKSearchBaseModel : GKHomeHotPaperModel
+
+@end
+//专题
+@interface GKSearchPojectModel : GKSearchBaseModel
+
+@end
+//铃声
+@interface GKSearchBellModel : GKSearchBaseModel
 
 
+@end
+//动态
+@interface GKSearchDynamicModel : GKSearchBaseModel
+
+@property (copy, nonatomic) NSString *comment;
+@property (copy, nonatomic) NSString *imgid;
+@property (copy, nonatomic) NSString *zip;
+
+
+@end
+//锁屏
+@interface GKSearchLockModel : GKSearchBaseModel
+
+@end
 
 NS_ASSUME_NONNULL_END
--- GKiOSApp/GKiOSApp/Class/Tool/ATBrowserTool.h
+++ GKiOSApp/GKiOSApp/Class/Tool/ATBrowserTool.h
@@ -12,7 +12,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface ATBrowserTool : NSObject
 
-+ (instancetype)browserTools:(NSArray *)browserTools selectIndex:(NSInteger)selectIndex;
+//+ (instancetype)browserTools:(NSArray *)browserTools selectIndex:(NSInteger)selectIndex;
 
 @end
 
