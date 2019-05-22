adf198228af629a216672716ee66810e06a05eda
wangws1990 <wangws1990@163.com>
2019-05-22 21:15:42 +0800

    bug

---
+++ GKiOSApp/GKiOSApp/Class/Tool/VTContentView+GKBack.h
@@ -0,0 +1,17 @@
+//
+//  VTContentView+GKBack.h
+//  GKiOSApp
+//
+//  Created by wangws1990 on 2019/5/22.
+//  Copyright © 2019 wangws1990. All rights reserved.
+//
+
+
+#import "VTContentView.h"
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VTContentView (GKBack)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GKiOSApp/GKiOSApp/Class/Tool/VTContentView+GKBack.m
@@ -0,0 +1,20 @@
+//
+//  VTContentView+GKBack.m
+//  GKiOSApp
+//
+//  Created by wangws1990 on 2019/5/22.
+//  Copyright © 2019 wangws1990. All rights reserved.
+//
+
+#import "VTContentView+GKBack.h"
+
+@implementation VTContentView (GKBack)
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    if (self.contentOffset.x <= 0) {
+        if ([otherGestureRecognizer.delegate isKindOfClass:NSClassFromString(@"_FDFullscreenPopGestureRecognizerDelegate")]) {
+            return YES;
+        }
+    }
+    return NO;
+}
+@end
